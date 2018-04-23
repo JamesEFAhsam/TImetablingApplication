9f88c95b6de398466b018f910246575bf1a32c3e
R1chard0418 <36198383+R1chard0418@users.noreply.github.com>
2018-04-23 19:16:32 +0100

    Update conflictChecker.js

--- smartrota/conflictChecker.js
+++ smartrota/conflictChecker.js
@@ -10,8 +10,6 @@ function conflictChecker(){
   });
   numOfRanks += 1;
 
-  console.log(numOfRanks);
-
 /*
      Iterating through the array, add up the number of managers and employees.
      And add up the days that they can't work.
@@ -22,18 +20,19 @@ function conflictChecker(){
       managerNum += 1;
       if(typeof person.getConstraint(2).data !== "undefined"){
         for(var i=0; i<person.getConstraint(2).data.length; i++){
-          managerCheck[person.getConstraint(2)[i]-1] += 1;
+          managerCheck[person.getConstraint(2).data[i]] += 1;
         }
       }
-    }else if(person.getConstraint(0) == 0){
+    }else if(person.getConstraint(0).data == 0){
       employeeNum += 1;
       if(typeof person.getConstraint(2).data !== "undefined"){
-        for(var i=0; i<person.getConstraint(2).length; i++){
-          employeeCheck[person.getConstraint(2)[i]-1] += 1;
+        for(var i=0; i<person.getConstraint(2).data.length; i++){
+          employeeCheck[person.getConstraint(2).data[i]] += 1;
         }
       }
     }
   });
+  
   managerCheck.forEach(function(day){
     if(day == managerNum) return false;
   });
@@ -45,8 +44,12 @@ function conflictChecker(){
   var totalHours; // Decided by the client (We'll do 2 shifts per day, 7 hours per shift for now)
   var pplperShift; // How many employees needed for one week
   var workHours = 0;
-  people.forEach(function(person){
+  people.forEach(function(person) {
     if(person.getConstraint(0).data == 0) workHours += person.getConstraint(1).data;
   });
-  if(workHours/pplperShift < totalHours) return false;
+  console.log(workHours);
+  if(workHours/pplperShift < totalHours) {
+    return false;
+  }
+  return true;
 }
